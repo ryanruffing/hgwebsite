8f0edab03d896a916eb73a98218af33242bc4ea5
rruffing@wynhouse.co <rruffing@wynhouse.co>
2024-02-14 18:59:46 -0500

    fixes for ui

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import genie from "./genietm.png";
 import { database } from "./firebase";
 import { ref, get } from "firebase/database";
 import { Helmet } from "react-helmet";
-
 import {
   Text,
   Image,
   Center,
   Input,
   Button,
-  HStack,
   VStack,
+  FormControl,
+  FormLabel,
   Box,
   Modal,
   ModalOverlay,
@@ -125,10 +125,12 @@ function App() {
         />
       </Helmet>
 
-      <Center minH="100vh" flexDirection="column">
+      <Center minH="75vh" flexDirection="column">
         <Box position="absolute" top="20px" left="20px">
           <Button
+            height={"100%"}
             fontFamily="Arial"
+            fontSize={[100, 75, 50, 24]}
             bgColor="#FF4E00"
             color="black"
             borderColor="#FF4E00"
@@ -138,46 +140,70 @@ function App() {
             About
           </Button>
         </Box>
+
+        <Image
+          src={genie}
+          maxW={{ base: "20", md: "80%", lg: "30%" }}
+          mt={25}
+          mb={50}
+        />
+        <Text
+          px={10}
+          fontSize={[100, 75, 50, 24]}
+          fontWeight={"bold"}
+          align={"center"}
+        >
+          Subscribe to receive arrival updates and free hitting advice.
+        </Text>
         {success && (
-          <Text color={"green"} fontSize={25} pt={20} ml={10} mr={10}>
+          <Text
+            color={"green"}
+            fontSize={[100, 75, 50, 24]}
+            align={"center"}
+            pt={20}
+            ml={10}
+            mr={10}
+          >
             Successfully subscribed! We will be in touch when updates appear.
           </Text>
         )}
-
-        <Image src={genie} maxW={{ base: "20", md: "80%", lg: "30%" }} mt={25} mb={50} />
-        <Text mb={20} px={10} fontSize={32} fontWeight={"bold"} align={"center"}>
-          Subscribe to receive arrival updates and free hitting advice.
-        </Text>
-        <VStack spacing={2}>
-          {" "}
-          {/* Use VStack for vertical stack on small screens */}
-          <Input
-            backgroundColor={"black"}
-            fontFamily={"arial"}
-            textColor={"white"}
-            placeholder="First Name"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            focusBorderColor="#FF4E00"
-            placeholderTextColor="white"
-            maxW={{ base: "100", md: "100%", lg: "100%" }}// Adjust width based on screen size
-          />
-          <Input
-            backgroundColor={"black"}
-            fontFamily={"arial"}
-            textColor={"white"}
-            placeholder="Last Name"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            focusBorderColor="#FF4E00"
-            placeholderTextColor="white"
-            maxW={{ base: "100", md: "100%", lg: "100%" }} // Adjust width based on screen size
-          />
-          <HStack spacing={0} width="100%">
-            {" "}
-            {/* Use HStack for horizontal stack */}
+        <VStack spacing={5} width={["100%", "70%", "95%", "35%", "35%"]}>
+          <FormControl isRequired>
+            <FormLabel fontSize={[100, 75, 50, 24]}>First Name</FormLabel>
             <Input
+              height={"100%"}
               backgroundColor={"black"}
+              fontFamily={"arial"}
+              textColor={"white"}
+              fontSize={[100, 75, 50, 24]}
+              placeholder="First Name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              focusBorderColor="#FF4E00"
+              placeholderTextColor="white"
+            />
+          </FormControl>
+          <FormControl id="lastName" isRequired>
+            <FormLabel fontSize={[100, 75, 50, 24]}>Last Name</FormLabel>
+            <Input
+              height={"100%"}
+              backgroundColor={"black"}
+              fontFamily={"arial"}
+              fontSize={[100, 75, 50, 24]}
+              textColor={"white"}
+              placeholder="Last Name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              focusBorderColor="#FF4E00"
+              placeholderTextColor="white"
+            />
+          </FormControl>
+          <FormControl id="email" isRequired>
+            <FormLabel fontSize={[100, 75, 50, 24]}>Email Address</FormLabel>
+            <Input
+              height={"100%"}
+              backgroundColor={"black"}
+              fontSize={[100, 75, 50, 24]}
               fontFamily={"arial"}
               textColor={"white"}
               type="email"
@@ -186,41 +212,56 @@ function App() {
               onChange={(e) => setEmail(e.target.value)}
               focusBorderColor="#FF4E00"
               placeholderTextColor="white"
-              width={{ base: "80%", md: "auto" }} // Adjust width based on screen size
             />
-            <Button
-              fontFamily={"Arial"}
-              onClick={handleSaveEmail}
-              bgColor="#FF4E00"
-              color="black"
-              borderColor="#FF4E00"
-              borderRadius="5"
-              width={{ base: "80%", md: "auto" }} // Adjust width based on screen size
+          </FormControl>
+          {error && (
+            <Text
+              color="red"
+              fontSize={[100, 75, 50, 24]}
+              fontWeight={"bold"}
+              align={"center"}
             >
-              Sign Up
-            </Button>
-          </HStack>
+              {errorMsg}
+            </Text>
+          )}
+          <Button
+            height={"100%"}
+            fontSize={[100, 75, 50, 24]}
+            fontFamily={"Arial"}
+            onClick={handleSaveEmail}
+            bgColor="#FF4E00"
+            color="black"
+            borderColor="#FF4E00"
+            borderRadius="5"
+            width={["100%", "70%", "95%", "70%", "70%"]}
+            mt={10}
+          >
+            Sign Up
+          </Button>
         </VStack>
 
-        {error && <Text color="red">{errorMsg}</Text>}
-        <Modal isOpen={isModalOpen} onClose={handleClose}>
+        <Modal isOpen={isModalOpen} maxH={"100hv"} onClose={handleClose}>
           <ModalOverlay />
-          <ModalContent maxW="90vw" maxH="90vh">
-            <ModalCloseButton />
+          <ModalContent maxW="100vw" maxH="100%">
+            <ModalCloseButton size={"100%"} />
             <ModalBody>
               <Center>
-                <Image src={genie} maxW={150} mb={4} />
+                <Image
+                  src={genie}
+                  maxW={{ base: "20", md: "50%", lg: "30%" }}
+                  mb={4}
+                />
               </Center>
-              <ModalHeader textAlign="center">
+              <ModalHeader fontSize={[100, 75, 50, 24]} textAlign="center">
                 <u>About</u>
               </ModalHeader>{" "}
               <Box
-                maxHeight="300px"
+                maxHeight={["1000px", "900px", "700px", "300px", "200px"]}
                 overflowY="auto"
                 padding="0.5rem"
                 whiteSpace="pre-line"
               >
-                <Text>{aboutContent}</Text>
+                <Text fontSize={[100, 75, 50, 24]}>{aboutContent}</Text>
               </Box>
             </ModalBody>
           </ModalContent>
